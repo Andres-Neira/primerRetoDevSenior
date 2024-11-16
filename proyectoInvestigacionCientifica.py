d2c39507bd99562abc2c99be6c035950bccd5c5b
Andres-Neira <andres.neira.pa@gmail.com>
2024-11-16 15:43:20 -0500

    Arquitectura de datos
    Forma como se plantea el codigó

---
+++ proyectoInvestigacionCientifica.py
@@ -0,0 +1,30 @@
+# Importar librerias de fechas
+import datetime
+
+#Permite agregar experimentos
+def  agregarExperimetos():
+    #Se deben validar los datos para que no se cometan errores en el ingreso de inormación
+    #Atributos : nombre, fecha, tipo y resultados numéricos.
+    pass
+
+#Permite visualizar los experimentos
+def visualizarExperimentos():
+    pass
+
+#Analizar datos
+def calcularDatos():
+    # calcular promedio , maximo , minimo
+    pass
+
+#Generación de informe final
+def generarInforme():
+    pass
+
+# menu para interactuar
+def menu():
+    pass
+
+# iniciador de programa
+def def __init__=='__main__'
+  self.name = name
+  self.age = age
